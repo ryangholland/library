9bafdca38cad405d246f416e249040352691610f
ryangholland <rghcode@gmail.com>
2023-04-30 17:15:50 -0400

    Create addBook function to generate new book objects with form data

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const contentDiv = document.querySelector(".content");
+const addBookBtn = document.querySelector(".add-book");
+const newBookForm = document.querySelector(".new-book-form");
+
+const newTitleInput = document.querySelector(".new-title");
+const newAuthorInput = document.querySelector(".new-author");
+const newPagesInput = document.querySelector(".new-pages");
+const bookRead = document.querySelector(".read-true");
 
 const myLibrary = [
   {
@@ -28,11 +35,6 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-}
-
 function displayBook(book) {
   const bookDiv = document.createElement("p");
   const titleText = document.createElement("h3");
@@ -57,5 +59,22 @@ function displayBook(book) {
 function displayAllBooks(library) {
   library.forEach((book) => displayBook(book));
 }
+
+function addBook() {
+  const newTitle = newTitleInput.value;
+  const newAuthor = newAuthorInput.value;
+  const newPages = newPagesInput.value;
+  const newRead = bookRead.checked;
+  const newBook = new Book(newTitle, newAuthor, newPages, newRead);
+
+  myLibrary.push(newBook);
+  displayBook(newBook);
+}
+
+newBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addBook();
+  newBookForm.reset();
+});
 
 displayAllBooks(myLibrary);
